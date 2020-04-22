eb9e180cde7a7f88a117ec48560eb39de3ba88fb
Edmilson Rocha <edmilsonrobson@gmail.com>
2020-04-22 03:13:20 -0300

    Fixes name of TwitchManager test class

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,23 @@ import PodcastManager from "./managers/PodcastManager";
 import TwitchManager from "./managers/TwitchManager";
 import Config from "./config";
 
-const run = async () => {
-  /* const manager = new TwitchManager({
-    twitchClientId: "7t8w5vr1dzmpstdjj37xtsyrw7il8z",
-    twitchUserId: "36830116",
-  });
+const manager = new PodcastManager();
+const twitchManager = new TwitchManager({
+  twitchClientId: "7t8w5vr1dzmpstdjj37xtsyrw7il8z",
+  twitchUserId: "36830116",
+});
 
-  const videos = await manager.fetchVideos(); */
+const testVar = async () => {
+  /* const result = await twitchManager.fetchVideos();
+    console.log(result); */
+  try {
+    const result = await manager.fetchGames();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-run();
+testVar();
 
 export { PodcastManager, Config, TwitchManager };
